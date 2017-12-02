8d8ed144d030ad95a14e676ff19fd79050a6c870
Jez Cope <j.s.cope@sheffield.ac.uk>
2017-12-02 09:41:05 +0000

    Day 2 golfing

--- 02-corruption-checksum.py
+++ 02-corruption-checksum.py
@@ -1,13 +1,9 @@
 from fileinput import input
 
-sheet = [[int(x) for x in l.split()] for l in input()]
+sheet = [sorted(int(x) for x in l.split()) for l in input()]
 
 # Part 1
-print(sum(abs(max(x) - min(x)) for x in sheet))
+print(sum(x[-1] - x[0] for x in sheet))
 
 # Part 2
-def rowsum_div(row):
-    row = sorted(row)
-    return sum(y // x for i, x in enumerate(row) for y in row[i+1:] if y % x == 0)
-
-print(sum(rowsum_div(x) for x in sheet))
+print(sum(y // x for row in sheet for i, x in enumerate(row) for y in row[i+1:] if y % x == 0))
