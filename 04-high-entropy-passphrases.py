b6e4ef709d67f6a914de0059644ee7d478928270
Jez Cope <j.s.cope@sheffield.ac.uk>
2017-12-05 07:48:04 +0000

    Tweak previous solution to day 4

--- 04-high-entropy-passphrases.py
+++ 04-high-entropy-passphrases.py
@@ -9,9 +9,9 @@ def is_valid(passphrase):
 
 def is_valid_ana(passphrase):
     counter = Counter(''.join(sorted(word)) for word in passphrase.split())
-
     return counter.most_common(1)[0][1] == 1
 
 
-print(sum(map(is_valid, input())))
-print(sum(map(is_valid_ana, input())))
+lines = list(input())
+print(sum(map(is_valid, lines)))
+print(sum(map(is_valid_ana, lines)))
