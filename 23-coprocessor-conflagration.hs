b886069a355e3a6dcdad4737d3a1a2155ad4106a
Jez Cope <j.s.cope@sheffield.ac.uk>
2017-12-24 16:39:25 +0000

    Improve Day 23 based on advice from reddit
    https://www.reddit.com/r/haskell/comments/7lnrvv/code_review_parsing_state_monad_and_strict/

--- 23-coprocessor-conflagration.hs
+++ 23-coprocessor-conflagration.hs
@@ -11,36 +11,44 @@ import Control.Monad.Loops
 import Text.ParserCombinators.Parsec hiding (State)
 import Debug.Trace
 
-data CoproVal = Reg Char | Val Int deriving Show
--- type CoproQueue = [Int]
-data CoproInstruction =  Set CoproVal CoproVal
-                       | Sub CoproVal CoproVal
-                       | Mul CoproVal CoproVal
-                       | Jnz CoproVal CoproVal
-                        deriving Show
-type CoproProgram = V.Vector CoproInstruction
+type Register = Char
+type Value = Int
+type Argument = Either Value Register
+data Instruction = Set Register Argument
+                 | Sub Register Argument
+                 | Mul Register Argument
+                 | Jnz Argument Argument
+  deriving Show
+type Program = V.Vector Instruction
+data Result = Cont | Halt deriving (Eq, Show)
 
-type CoproRegisters = M.Map Char Int
-data Copro = Copro { dRegisters :: CoproRegisters
-                 , dPtr :: Int
-                 , dMulCount :: Int
-                 , dProgram :: CoproProgram }
+type Registers = M.Map Char Int
+data Machine = Machine { dRegisters :: Registers
+                       , dPtr :: !Int
+                       , dMulCount :: !Int
+                       , dProgram :: Program }
 
-instance Show Copro where
+instance Show Machine where
   show d = show (dRegisters d) ++ " @" ++ show (dPtr d) ++ " ×" ++ show (dMulCount d)
 
-defaultCopro = Copro M.empty 0 0 V.empty
+defaultMachine = Machine M.empty 0 0 V.empty
 
-type CoproState = State Copro
+type MachineState = State Machine
 
-program :: GenParser Char st CoproProgram
+program :: GenParser Char st Program
 program = do
   instructions <- endBy instruction eol
   return $ V.fromList instructions
   where
-    instruction = try (twoArg "set" Set) <|> twoArg "sub" Sub
-                  <|> twoArg "mul" Mul <|> twoArg "jnz" Jnz
-    twoArg n c = do
+    instruction = try (regOp "set" Set) <|> regOp "sub" Sub
+                  <|> regOp "mul" Mul <|> jump "jnz" Jnz
+    regOp n c = do
+      string n >> spaces
+      val1 <- oneOf "abcdefgh"
+      spaces
+      val2 <- regOrVal
+      return $ c val1 val2
+    jump n c = do
       string n >> spaces
       val1 <- regOrVal
       spaces
@@ -49,88 +57,87 @@ program = do
     regOrVal = register <|> value
     register = do
       name <- lower
-      return $ Reg name
+      return $ Right name
     value = do
       val <- many $ oneOf "-0123456789"
-      return $ Val $ read val
+      return $ Left $ read val
     eol = char '\n'
 
-parseProgram :: String -> Either ParseError CoproProgram
+parseProgram :: String -> Either ParseError Program
 parseProgram = parse program ""
 
-getReg :: Char -> CoproState Int
+getReg :: Char -> MachineState Int
 getReg r = do
   st <- get
   return $ M.findWithDefault 0 r (dRegisters st)
 
-putReg :: Char -> Int -> CoproState ()
+putReg :: Char -> Int -> MachineState ()
 putReg r v = do
   st <- get
   let current = dRegisters st
       new = M.insert r v current
   put $ st { dRegisters = new }
 
-modReg :: (Int -> Int -> Int) -> Char -> CoproVal -> CoproState Bool
+modReg :: (Int -> Int -> Int) -> Char -> Argument -> MachineState ()
 modReg op r v = do
   u <- getReg r
   v' <- getRegOrVal v
   putReg r (u `op` v')
   incPtr
-  return False
 
-getRegOrVal :: CoproVal -> CoproState Int
-getRegOrVal (Reg r) = getReg r
-getRegOrVal (Val v) = return v
+getRegOrVal :: Argument -> MachineState Int
+getRegOrVal = either return getReg
 
-addPtr :: Int -> CoproState ()
+addPtr :: Int -> MachineState ()
 addPtr n = do
   st <- get
   put $ st { dPtr = n + dPtr st }
 
 incPtr = addPtr 1
 
-execInst :: CoproInstruction -> CoproState Bool
-execInst (Set (Reg reg) val) = do
+execInst :: Instruction -> MachineState ()
+execInst (Set reg val) = do
   newVal <- getRegOrVal val
   putReg reg newVal
   incPtr
-  return False
-execInst (Mul (Reg reg) val) = do
+execInst (Mul reg val) = do
   result <- modReg (*) reg val
   st <- get
   put $ st { dMulCount = 1 + dMulCount st }
   return result
-execInst (Sub (Reg reg) val) = modReg (-) reg val
+execInst (Sub reg val) = modReg (-) reg val
 execInst (Jnz val1 val2) = do
   st <- get
   test <- getRegOrVal val1
   jump <- if test /= 0 then getRegOrVal val2 else return 1
   addPtr jump
-  return False
-execInst x = error $ "execInst not implemented yet for " ++ show x
 
-execNext :: CoproState Bool
+execNext :: MachineState Result
 execNext = do
   st <- get
   let prog = dProgram st
       p = dPtr st
-  if p >= length prog then return True else do
-    result <- execInst (prog V.! p)
-    return result
+  if p >= length prog then return Halt else do
+    execInst (prog V.! p)
+    return Cont
 
-runUntilTerm :: CoproState ()
+runUntilTerm :: MachineState ()
 runUntilTerm = do
-  terminate <- execNext
-  unless terminate runUntilTerm
+  result <- execNext
+  unless (result == Halt) runUntilTerm
 
 optimisedCalc :: Int -> Int -> Int -> Int
 optimisedCalc a b k = sum $ map (const 1) $ filter notPrime [a,a+k..b]
   where
-    notPrime n = any (==0) $ map (mod n) [2..n-1]
+    notPrime n = any (==0) $ map (mod n) [2..(floor $ sqrt $ fromIntegral n)]
 
 main = do
-  prog <- fmap (fromRight V.empty . parseProgram) getContents  
-  let c = defaultCopro { dProgram = prog }
-      (_, c') = runState runUntilTerm c
-  putStrLn $ show (dMulCount c') ++ " multiplications made"
-  putStrLn $ "Calculation result: " ++ show (optimisedCalc 107900 124900 17)
+  input <- getContents  
+  case parseProgram input of
+    Right prog -> do
+      let c = defaultMachine { dProgram = prog }
+          (_, c') = runState runUntilTerm c
+      putStrLn $ show (dMulCount c') ++ " multiplications made"
+      putStrLn $ "Calculation result: " ++ show (optimisedCalc 107900 124900 17)
+    Left error -> do
+      print error
